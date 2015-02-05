c85174257ca7aae707afa2ed51490a55b8cecd96
Damian <d.alvarez.mallon@fz-juelich.de>
2015-02-05 19:56:46 +0100

    Uncommented the conditional include of mm_malloc.h (Why was it commented?)

--- include/Alloc.h
+++ include/Alloc.h
@@ -3,9 +3,9 @@
 #include <cstddef> // for alignment stuff
 #include "asserts.h" // for assert_le, assert_lt
 #include "arraysfwd.h"
-//#ifndef __INTEL_COMPILER
+#ifndef __INTEL_COMPILER
   #include <mm_malloc.h>
-//#endif
+#endif
 //#include "arrays.h" // fixed-dimension arrays
 
 /*

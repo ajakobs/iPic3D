921be9fac2ae5ea77298149ff97137e8678acbc1
Anna <a.jakobs@fz-juelich.de>
2015-06-03 10:30:09 +0200

    Adapted mn3_m2x_native.lsf, now able to run on more than 1 node

--- scripts/run_fields.sh
+++ scripts/run_fields.sh
@@ -2,6 +2,6 @@
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/apps/intel/13/composer_xe_2013_sp1.1.106/compiler/lib/intel64
 # Set number of OpenMP threads per spawned MPI proc
 #export NX_ARGS="--smp-workers=2"
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=2
 exec ../build.xeon/iPic3D.intel64 $1
 
